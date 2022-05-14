43c372d48316aec01c0aa0014328e81049c519e1
priynashu <72725684+priynashu@users.noreply.github.com>
2022-05-14 15:46:02 +0530

    make it better 4

--- index.js
+++ index.js
@@ -1,117 +1,46 @@
-// const express = require("express");
-// const mongoose = require("mongoose");
-// // const morgan = require("morgan");
-// const cors = require("cors");
-// const bodyParser = require("body-parser");
-// // const { readdirSync } = require("fs"); //destructring
-// // const { UserSchema } = require("./models/user");
-// var http = require('http');
-// var fs = require('fs');
-// require("dotenv").config();
-// //app
-// const app = express();
-// app.use(express.urlencoded({ limit: '25mb', extended: true }));
-// app.use(express.json({ limit: '25mb' }));
-// app.use(cors());
-
-// //import
-// // const authRoute = require("./routes/auth");
-// //database
-// mongoose
-//   .connect("mongodb+srv://pmjshah11:Priyanshu11@cluster0.4i5uu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", {
-//     useNewUrlParser: true,
-//   })
-//   .then(() => {
-//     console.log(`DB connection is successfully connected`);
-//   })
-//   .catch((err) => console.log(`DB connection err are ${err}`));
-  
-// //middlewares
-// app.use(bodyParser.json({ limit: "2mb" })); // here the data sent by kind
-// // will converterd to json and limit stands not more than 2mb
-// // app.use(morgan("dev"));
-
-// //routes middlewares
-//  //here api is prefix like /api/other routes
-
-// app.get('/', (req, res) => {  
-//     // Print the name of the file for which request is made.
-//     console.log("Request for demo file received.",__dirname);
-//     res.json(<h1>Priyanshu Shah Is here</h1>)
-//     // fs.readFile("./htmlFiles/index.html",function(error, data){
-//     //   if (error) {
-//     //     res.writeHead(404);
-//     //     res.write('Contents you are looking for-not found');
-//     //     res.end();
-//     //   }  else {
-//     //     res.writeHead(200, {
-//     //       'Content-Type': 'text/html'
-//     //     });
-//     //     res.write(data.toString());
-//     //     res.end();
-//     //   }
-//     // });
-//   });
-// // app.get('/functions', (req, res) => {  
-// //     // Print the name of the file for which request is made.
-// //     console.log("Request for demo file received.");
-// //     fs.readFile("./htmlFiles/functions.html",function(error, data){
-// //       if (error) {
-// //         res.writeHead(404);
-// //         res.write('Contents you are looking for-not found');
-// //         res.end();
-// //       }  else {
-// //         res.writeHead(200, {
-// //           'Content-Type': 'text/html'
-// //         });
-// //         res.write(data.toString());
-// //         res.end();
-// //       }
-// //     });
-// //   });
-
-//   // var crypto = require("crypto-js");
-  
-//   // console.log(crypto.SHA1().toString());
-// const api = require('./routes/apis');
-// app.use('/api',api);
-// //it will read all the files from routes folder and .map() will help to require every route so we
-// //dont need app.use every time
-
-// //PORT
-// const port = process.env.PORT || 8000;
-// app.listen(port, () => {
-//   console.log("server is successfully running on", port);
-// });
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-
-const dotenv = require('dotenv');
-dotenv.config();
-
-
-
-// const schedule = require('node-schedule')
-
-
+const express = require("express");
+const mongoose = require("mongoose");
+// const morgan = require("morgan");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+// const { readdirSync } = require("fs"); //destructring
+// const { UserSchema } = require("./models/user");
+var http = require('http');
+var fs = require('fs');
+require("dotenv").config();
+//app
 const app = express();
-
 app.use(express.urlencoded({ limit: '25mb', extended: true }));
 app.use(express.json({ limit: '25mb' }));
 app.use(cors());
 
-// console.log("dirname is",__dirname);
-var http = require('http');
-var fs = require('fs');
+//import
+// const authRoute = require("./routes/auth");
+//database
+mongoose
+  .connect("mongodb+srv://pmjshah11:Priyanshu11@cluster0.4i5uu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", {
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    console.log(`DB connection is successfully connected`);
+  })
+  .catch((err) => console.log(`DB connection err are ${err}`));
+  
+//middlewares
+app.use(bodyParser.json({ limit: "2mb" })); // here the data sent by kind
+// will converterd to json and limit stands not more than 2mb
+// app.use(morgan("dev"));
 
-app.get('/', (req, res) => {  
+//routes middlewares
+ //here api is prefix like /api/other routes
+
+ app.get('/', (req, res) => {  
   // Print the name of the file for which request is made.
   // console.log("Request for salesroom file.");
-  res.json({
-    name:"Priyanshu Shah"
-  })
-  fs.readFile(__dirname+"/htmlFiles/login.html",function(error, data){
+  // res.json({
+  //   name:"Priyanshu Shah"
+  // })
+  fs.readFile("./htmlFiles/login.html",function(error, data){
     if (error) {
       res.writeHead(404);
       res.write('Contents you are looking for-not found webinar');
@@ -125,17 +54,35 @@ app.get('/', (req, res) => {
     }
   });
 });
-const port = process.env.PORT || 8000;
+// app.get('/functions', (req, res) => {  
+//     // Print the name of the file for which request is made.
+//     console.log("Request for demo file received.");
+//     fs.readFile(__dirname+"/htmlFiles/functions.html",function(error, data){
+//       if (error) {
+//         res.writeHead(404);
+//         res.write('Contents you are looking for-not found');
+//         res.end();
+//       }  else {
+//         res.writeHead(200, {
+//           'Content-Type': 'text/html'
+//         });
+//         res.write(data.toString());
+//         res.end();
+//       }
+//     });
+//   });
+
+  // var crypto = require("crypto-js");
+  
+  // console.log(crypto.SHA1().toString());
+const api = require('./routes/apis');
+app.use('/api',api);
+//it will read all the files from routes folder and .map() will help to require every route so we
+//dont need app.use every time
+
+//PORT
+// const port = process.env.PORT || 8000;
+const port = 8000
 app.listen(port, () => {
   console.log("server is successfully running on", port);
 });
-mongoose
-  .connect(process.env.MONGODB_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log(`DB connection is successfully connected`);
-  })
-  .catch((err) => console.log(`DB connection err are ${err}`));
-  
